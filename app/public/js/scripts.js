6b4e3f6df04041ed382b89b29affdcdefddc74c1
Ruslan <ruslan.rusu@yahoo.com>
2020-04-15 18:54:40 -0400

    storage wired

--- app/public/js/scripts.js
+++ app/public/js/scripts.js
@@ -1,3 +1,15 @@
 var title = new MediumEditor('.art-title');
+title.subscribe('editableInput', function (event, editorElement) {
+	var titleText = editorElement.innerText;
+  	
+  	console.log("title: " + titleText)
+  	store('title', titleText); 
+});
 
 var content = new MediumEditor('.art-content');
+content.subscribe('editableInput', function (event, editorElement) {
+	var bodyText = editorElement.innerText;
+  	
+  	console.log("body: " + bodyText)
+  	store('body', bodyText)
+});
