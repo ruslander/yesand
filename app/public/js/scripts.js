401779864c40d9a8a90b3341aaf17abe972e0c36
Ruslan <ruslan.rusu@yahoo.com>
2020-04-16 09:58:15 -0400

    layout

--- app/public/js/scripts.js
+++ app/public/js/scripts.js
@@ -1,32 +1,51 @@
-var title = "Yes, And";
-var content = "The 'Yes' portion of the rule encourages the acceptance of the contributions added by others. Participants in an improvisation are encouraged to agree to proposition, fostering a sense of cooperation[1] rather than shutting down the suggestion and effectively ending the line of communication. More here https://en.wikipedia.org/wiki/Yes,_and...";
+var defaultTitle = "Yes, And ...";
+var defaultContent = "The 'Yes' portion of the rule encourages the acceptance of the contributions added by others. Participants in an improvisation are encouraged to agree to proposition, fostering a sense of cooperation[1] rather than shutting down the suggestion and effectively ending the line of communication. More here https://en.wikipedia.org/wiki/Yes,_and...";
+
+var title;
+var content;
+
+function calReadTime(text){
+	var wordCount = text.replace( /[^\w ]/g, "" ).split( /\s+/ ).length;
+
+    var readingTimeInMinutes = Math.floor(wordCount / 228) + 1;
+    return readingTimeInMinutes + " min";
+}
 
 function autoSave() {
 
-  	$("#saved").text(new Date().toLocaleTimeString())
-  	$("#words").text(content.split(" ").length)
+  	$("#saved").text(new Date().toLocaleTimeString());
+  	$("#words").text(content.split(" ").length);
+  	$("#reading-time").text(calReadTime(content));
 
   	store.set('title', title); 
-  	store.set('content', content)
+  	store.set('content', content);
 }
 
 $(function() {
-	var recTitle = store.get('title') != null ? store.get('title') : title;
-	var recContent = store.get('content') != null ? store.get('content') : content;
+	title = store.get('title') != null ? store.get('title') : defaultTitle;
+	content = store.get('content') != null ? store.get('content') : defaultContent;
 
-  	$(".art-title").text(recTitle)
-  	$(".art-content").text(recContent)
+  	$(".art-title").text(title);
+  	$(".art-content").text(content);
+
+  	$("#new-article").click(function() {
+  		title = defaultTitle;
+  		content = defaultContent;
+
+        $(".art-title").text(defaultTitle);
+  		$(".art-content").text(defaultContent);
+    });
 
 	var titleEditor = new MediumEditor('.art-title');
 	titleEditor.subscribe('editableInput', function (event, editorElement) {
 		title = editorElement.innerText;
-	  	console.log("title: " + title)
+	  	console.log("title: " + title);
 	});
 
 	var contentEditor = new MediumEditor('.art-content');
 	contentEditor.subscribe('editableInput', function (event, editorElement) {
 		content = editorElement.innerText;
-		console.log("body: " + content)
+		console.log("body: " + content);
 	});
 
   	setInterval(autoSave, 5000);
