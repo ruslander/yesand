cf48c02f167e9e3db81a87fafb9a13eca85243bc
Ruslan <ruslan.rusu@yahoo.com>
2020-04-16 08:26:53 -0400

    storage recovery

--- app/public/js/scripts.js
+++ app/public/js/scripts.js
@@ -1,31 +1,35 @@
-var title = "";
-var content = "";
-
-
-
-var titleEditor = new MediumEditor('.art-title');
-titleEditor.subscribe('editableInput', function (event, editorElement) {
-	title = editorElement.innerText;
-  	console.log("title: " + title)
-});
-
-var contentEditor = new MediumEditor('.art-content');
-contentEditor.subscribe('editableInput', function (event, editorElement) {
-	content = editorElement.innerText;
-	console.log("body: " + content)
-});
-
-
+var title = "Yes, And";
+var content = "The 'Yes' portion of the rule encourages the acceptance of the contributions added by others. Participants in an improvisation are encouraged to agree to proposition, fostering a sense of cooperation[1] rather than shutting down the suggestion and effectively ending the line of communication. More here https://en.wikipedia.org/wiki/Yes,_and...";
 
 function autoSave() {
-  	var d = new Date();
-  	var t = d.toLocaleTimeString();
-  	document.getElementById("saved").innerHTML = t;
 
-  	store('title', title); 
-  	store('body', content)
+  	$("#saved").text(new Date().toLocaleTimeString())
+  	$("#words").text(content.split(" ").length)
 
-  	document.getElementById('words').innerHTML = content.split(" ").length;
+  	store.set('title', title); 
+  	store.set('content', content)
 }
 
-setInterval(autoSave, 5000);
+$(function() {
+	var recTitle = store.get('title') != null ? store.get('title') : title;
+	var recContent = store.get('content') != null ? store.get('content') : content;
+
+  	$(".art-title").text(recTitle)
+  	$(".art-content").text(recContent)
+
+	var titleEditor = new MediumEditor('.art-title');
+	titleEditor.subscribe('editableInput', function (event, editorElement) {
+		title = editorElement.innerText;
+	  	console.log("title: " + title)
+	});
+
+	var contentEditor = new MediumEditor('.art-content');
+	contentEditor.subscribe('editableInput', function (event, editorElement) {
+		content = editorElement.innerText;
+		console.log("body: " + content)
+	});
+
+  	setInterval(autoSave, 5000);
+});
+
+
